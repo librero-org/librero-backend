fab67bc9b8835bf304110f4f37e5e3f71bae51f2
Neme Saenz <neme.saenz@wizeline.com>
2020-11-07 22:18:28 -0600

    chore(npm): add uplink-nodejs
    Bindings for storj

--- src/frameworks-drivers/storage/book-storage.ts
+++ src/frameworks-drivers/storage/book-storage.ts
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import storj from 'uplink-nodejs';
 import { BookStorage } from '../../entities/book/types';
 
 export class BookStorageImplementation implements BookStorage {
   async save(): Promise<{ url: string }> {
+    const libUplink = new storj.Uplink();
+
     return { url: 'localhost/files' };
   }
 }
