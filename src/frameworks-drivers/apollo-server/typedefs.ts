c20fb185b7e7e2da0098dfa9bd425df74785b320
Neme Saenz <neme.saenz@wizeline.com>
2020-11-16 11:32:01 -0600

    refactor(apollo-server/typedefs): get schema form graphql-codegen output
    So that the graphql files do not get merged twice and the prod dist can use a single file

--- src/frameworks-drivers/apollo-server/typedefs.ts
+++ src/frameworks-drivers/apollo-server/typedefs.ts
@@ -1,8 +1,9 @@
-import path from 'path';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
-const typesArray = loadFilesSync(path.join(__dirname, './**/*.gql'));
+// Get schema form graphql-codegen output
+// See ./graphql-codegen.yml
+const typesArray = loadFilesSync('./graphql-schema.graphql');
 const { definitions } = mergeTypeDefs(typesArray);
 
 export const typeDefs = { kind: 'Document' as const, definitions };
