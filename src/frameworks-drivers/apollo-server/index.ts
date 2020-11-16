64d0e2bc068320e92b5a5caab0136535b1f8d562
Neme Saenz <neme.saenz@wizeline.com>
2020-11-16 16:56:00 -0600

    feat(apollo-server): add logger plugin

--- src/frameworks-drivers/apollo-server/index.ts
+++ src/frameworks-drivers/apollo-server/index.ts
@@ -2,9 +2,11 @@ import { ApolloServer } from 'apollo-server';
 import { createContext } from './context';
 import { typeDefs } from './typedefs';
 import { resolvers } from './resolvers';
+import { loggerPlugin } from './logger';
 
 export const server = new ApolloServer({
   context: createContext,
+  plugins: [loggerPlugin],
   typeDefs,
   resolvers,
 });
