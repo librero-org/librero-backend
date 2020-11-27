406aad3750b9373e257ce2e9a74bdce7607747ec
Neme Saenz <neme.saenz@wizeline.com>
2020-11-27 15:12:55 -0600

    feat(prisma): add msg property to logger calls

--- src/frameworks-drivers/apollo-server/context.ts
+++ src/frameworks-drivers/apollo-server/context.ts
@@ -14,9 +14,13 @@ const prisma = new PrismaClient({
 });
 
 const prismaLogger = baseLogger.child({ origin: 'PrismaClient' });
-prisma.$on('query', (event) => prismaLogger.debug({ event }));
-prisma.$on('info', (event) => prismaLogger.info({ event }));
-prisma.$on('warn', (event) => prismaLogger.warn({ event }));
+prisma.$on('query', (event) =>
+  prismaLogger.debug({ event, msg: 'Prisma query' }),
+);
+prisma.$on('info', (event) => prismaLogger.info({ event, msg: 'Prisma info' }));
+prisma.$on('warn', (event) =>
+  prismaLogger.warn({ event, msg: 'Prisma warning' }),
+);
 
 const bookRepository = new BookRepository(prisma);
 
